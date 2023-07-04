906a73d509efe2d7b6c81e61eed8f0f3d4e4b304
suresh badikala <suba@odoo.com>
2023-07-04 10:08:13 +0530

    First Commit

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Learning management System',
+    'version': '1.0',
+    'category': 'Education',
+    'description': """The learning management (LSM) Application is a 
+    web-based platform designed to manage online educational courses.""",
+    
+    'depends': [],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/lms_menu_view.xml',
+    
+    ],
+    'installable': True,
+    'application' : True,
+}
---
+++ models/lms.py
@@ -0,0 +1,6 @@
+from odoo import fields, models
+
+class learning_system(models.Model):
+    _name = 'learning.system'
+    _description = 'Learning Management System'
+    _log_access = False
